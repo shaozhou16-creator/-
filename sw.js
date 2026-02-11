b874ac0a1726976c7aad8bb006f0dacf40faf374
shaozhou16-creator <shaozhou16@gmail.com>
2026-02-11 13:23:38 +0800

    style: optimize mobile UI layout and visual hierarchy

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE = 'quiz-app-v1';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return resp;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
